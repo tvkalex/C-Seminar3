7de030b61dab18cfe9bd2e263945fae1623b23ed
tvkalex <tvkalex@yandex.ru>
2022-12-07 23:22:10 +0300

    Add Task3 solution

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -2,3 +2,9 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+System.Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(System.Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    System.Console.Write($"{i * i * i} ");
+}
